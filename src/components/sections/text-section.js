06159910532084bb783cca60fcd663f99b6f7e0c
mostlyclicks <carlos@mostlyclicks.com>
2023-01-06 21:05:39 -0700

    add base styling for text-section.js

--- src/components/sections/text-section.js
+++ src/components/sections/text-section.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const TextSection = () => {
   return (
-    <section>
+    <StyledTextSection>
       <div>
         <p>
           This is hero text. Lorem ipsum dolor sit amet consectetur, adipisicing
           elit. Perspiciatis saepe distinctio dolores culpa laudantium
-          blanditiis nesciunt vitae voluptas? Ut dolor autem, labore quam
-          similique dolorem eum eaque necessitatibus maiores velit!
+          blanditiis nesciunt vitae voluptas!
         </p>
       </div>
-    </section>
+    </StyledTextSection>
   );
 }
 
 export default TextSection
+
+const StyledTextSection = styled.section`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #d8d8d8;
+  min-height:30vh;
+  div {
+    flex-basis:80%;
+  }
+
+  @media (min-width:768px) {
+    div {
+      flex-basis:40%;
+    }
+  }
+`;

aa7e85fde81465246960b232705b62a96e272541
mostlyclicks <carlos@mostlyclicks.com>
2023-01-06 20:54:15 -0700

    style hero section

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 import ReactLogo from '../images/logo2.svg'
 
 const Logo = () => {
   return (
-    <img src={ReactLogo} alt='Logo' />
+    <StyledImg src={ReactLogo} alt='Logo' />
   )
 }
 
 export default Logo
+
+const StyledImg = styled.img`
+  width:150px;
+  
+`
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import Logo from '../logo'
 import bgImg from '../../images/pexels-alex-andrews-3805983.jpg'
 
 const Hero = () => {
   return (
-    <section>
+    <StyledHeroSection>
       <Logo />
-      <h1>Welcome to sections</h1>
-      <img src={bgImg} alt='stars' />
-    </section>
+      <h1>Welcome to Sections</h1>
+    </StyledHeroSection>
   )
 }
 
 export default Hero
+
+
+const StyledHeroSection = styled.section`
+  background-image:url(${bgImg});
+  width:100%;
+  min-height:500px;
+  display:flex;
+  justify-content:center;
+  flex-direction:column;
+  align-items:center;
+  color:#ffffff;
+`
